8cfde30252db310f310d44f2e9107653048e2f62
kenjis <kenji.uui@gmail.com>
2015-05-14 21:23:33 +0900

    Remove all hook_call() in get_new_instance()
    Not all hooks were called, and timing of post_controller_constructor was wrong.

--- application/tests/_ci_phpunit_test/functions.php
+++ application/tests/_ci_phpunit_test/functions.php
@@ -32,8 +32,7 @@ function get_new_instance()
 
 	// Load core classes
 	load_class('Benchmark', 'core');
-	$EXT =& load_class('Hooks', 'core');
-	$EXT->call_hook('pre_system');
+	load_class('Hooks', 'core');
 	load_class('Config', 'core');
 //	load_class('Utf8', 'core');
 	load_class('URI', 'core');
@@ -42,13 +41,12 @@ function get_new_instance()
 	load_class('Security', 'core');
 	load_class('Input', 'core');
 	load_class('Lang', 'core');
-	$EXT->call_hook('pre_controller');
 	
 	$loader = new CITEST_Loader();
 	load_class_instance('Loader', $loader);
-	$EXT->call_hook('post_controller_constructor');
-
-	return new CI_Controller();
+	
+	$controller = new CI_Controller();
+	return $controller;
 }
 
 /**
