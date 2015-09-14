bcfe990160e5ecf1b089b93aea93589f720da6d4
kenjis <kenji.uui@gmail.com>
2015-09-14 13:16:23 +0900

    Remove unneeded variable

--- application/tests/_ci_phpunit_test/CIPHPUnitTestDouble.php
+++ application/tests/_ci_phpunit_test/CIPHPUnitTestDouble.php
@@ -80,7 +80,7 @@ class CIPHPUnitTestDouble
 				break;
 			case 5:
 				$invocation->$with(
-					$params[0], $params[1], $params[2], $params[3], $params[4], $params[5]
+					$params[0], $params[1], $params[2], $params[3], $params[4]
 				);
 				break;
 			default:
