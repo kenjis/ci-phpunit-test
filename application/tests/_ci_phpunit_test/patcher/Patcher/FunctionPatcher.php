20a1d8b1f375120cdaf1a40693e3ad1bec395be6
kenjis <kenji.uui@gmail.com>
2016-01-05 09:50:19 +0900

    Add functions to function patcher whitelist

--- application/tests/_ci_phpunit_test/patcher/Patcher/FunctionPatcher.php
+++ application/tests/_ci_phpunit_test/patcher/Patcher/FunctionPatcher.php
@@ -36,6 +36,8 @@ class FunctionPatcher extends AbstractPatcher
 		'microtime',
 		'date',
 		'function_exists',
+		'header',
+		'setcookie',
 		// Functions that have param called by reference
 		// Need to prepare method in FunctionPatcher\Proxy class
 		'openssl_random_pseudo_bytes',
