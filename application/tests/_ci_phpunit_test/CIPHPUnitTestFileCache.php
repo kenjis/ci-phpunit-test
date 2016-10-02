57b80102b42346265bbfc27e94317c24a537eaf9
kenjis <kenji.uui@gmail.com>
2016-10-03 07:08:50 +0900

    Now saves cache file only when updated

--- application/tests/_ci_phpunit_test/CIPHPUnitTestFileCache.php
+++ application/tests/_ci_phpunit_test/CIPHPUnitTestFileCache.php
@@ -12,6 +12,7 @@ class CIPHPUnitTestFileCache implements ArrayAccess
 {
 	private $file;
 	private $map = [];
+	private $updated = false;
 
 	public function __construct($file)
 	{
@@ -42,7 +43,9 @@ class CIPHPUnitTestFileCache implements ArrayAccess
 
 	public function __destruct()
 	{
-		file_put_contents($this->file, serialize($this->map));
+		if ($this->updated) {
+			file_put_contents($this->file, serialize($this->map));
+		}
 	}
 
 	/**
@@ -60,6 +63,7 @@ class CIPHPUnitTestFileCache implements ArrayAccess
 	public function offsetSet($key, $value)
 	{
 		$this->map[$key] = $value;
+		$this->updated = true;
 	}
 
 	public function offsetGet($key)
@@ -82,5 +86,6 @@ class CIPHPUnitTestFileCache implements ArrayAccess
 	public function offsetUnset($key)
 	{
 		unset($this->map[$key]);
+		$this->updated = true;
 	}
 }
