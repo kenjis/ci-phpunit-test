f7a1732032be847e0ff93e2bf1497d1d1bf85de8
kenjis <kenji.uui@gmail.com>
2017-02-06 08:39:44 +0900

    Change Welcome_test a bit

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -12,7 +12,7 @@ class Welcome_test extends TestCase
 {
 	public function test_index()
 	{
-		$output = $this->request('GET', '/');
+		$output = $this->request('GET', 'welcome/index');
 		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
 	}
 
