7273954efd9dc4a545cff67dc0ea2226a363b47c
kenjis <kenji.uui@gmail.com>
2018-03-18 09:30:34 +0900

    Fix coding style

--- install.php
+++ install.php
@@ -14,27 +14,34 @@ $silent = false;
 $app_dir = 'application';
 $pub_dir = 'public';
 
-if($argc > 1){
-    for ($i = 1; $i <= $argc; $i++){
-        if(!isset($argv[$i]))
+if ($argc > 1) {
+    for ($i = 1; $i <= $argc; $i++) {
+        if (! isset($argv[$i])) {
             break;
-        switch ($argv[$i]){
+        }
+
+        switch ($argv[$i]) {
             // php install.php -s
             case '-s':
                 $silent = true;
                 break;
+
             // php install.php -a application
             case '-a':
-                if(is_dir($argv[$i+1]))
+                if (is_dir($argv[$i+1])) {
                     $app_dir = $argv[$i+1];
+                }
                 $i++;
                 break;
+
             // php install.php -p public
             case '-p':
-                if(is_dir($argv[$i+1]))
+                if (is_dir($argv[$i+1])) {
                     $pub_dir = $argv[$i+1];
+                }
                 $i++;
                 break;
+
             default:
                 throw new Exception('Unknown argument: ' . $argv[$i]);
         }
