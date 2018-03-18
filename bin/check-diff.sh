a45709d2f76d55a2d21621c0eae01443d5e40f3b
kenjis <kenji.uui@gmail.com>
2018-03-18 13:16:59 +0900

    chore: refactor check-diff.sh

--- bin/check-diff.sh
+++ bin/check-diff.sh
@@ -3,13 +3,16 @@
 cd `dirname $0`
 cd ..
 
+system_dir="../../codeigniter/framework/system"
+replacing_dir="application/tests/_ci_phpunit_test/replacing"
+
 diff -u ../../codeigniter/framework/index.php application/tests/Bootstrap.php
 
-diff -u ../../codeigniter/framework/system/core/CodeIgniter.php application/tests/_ci_phpunit_test/replacing/core/CodeIgniter.php
-diff -u ../../codeigniter/framework/system/core/Input.php application/tests/_ci_phpunit_test/replacing/core/Input.php
-diff -u ../../codeigniter/framework/system/core/Loader.php application/tests/_ci_phpunit_test/replacing/core/Loader.php
+diff -u $system_dir/core/CodeIgniter.php $replacing_dir/core/CodeIgniter.php
+diff -u $system_dir/core/Input.php $replacing_dir/core/Input.php
+diff -u $system_dir/core/Loader.php $replacing_dir/core/Loader.php
 
-diff -u ../../codeigniter/framework/system/helpers/download_helper.php application/tests/_ci_phpunit_test/replacing/helpers/download_helper.php
-diff -u ../../codeigniter/framework/system/helpers/url_helper.php application/tests/_ci_phpunit_test/replacing/helpers/url_helper.php
+diff -u $system_dir/helpers/download_helper.php $replacing_dir/helpers/download_helper.php
+diff -u $system_dir/helpers/url_helper.php $replacing_dir/helpers/url_helper.php
 
-diff -u ../../codeigniter/framework/system/libraries/Upload.php application/tests/_ci_phpunit_test/replacing/libraries/Upload.php
+diff -u $system_dir/libraries/Upload.php $replacing_dir/libraries/Upload.php
