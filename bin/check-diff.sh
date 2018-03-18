93828aa29c0be49e941f210d6a7b0cc7e0e83e37
kenjis <kenji.uui@gmail.com>
2018-03-18 13:12:22 +0900

    chore: add empty lines

--- bin/check-diff.sh
+++ bin/check-diff.sh
@@ -4,7 +4,9 @@ cd `dirname $0`
 cd ..
 
 diff -u ../../codeigniter/framework/index.php application/tests/Bootstrap.php
+
 diff -u ../../codeigniter/framework/system/core/CodeIgniter.php application/tests/_ci_phpunit_test/replacing/core/CodeIgniter.php
 diff -u ../../codeigniter/framework/system/core/Input.php application/tests/_ci_phpunit_test/replacing/core/Input.php
 diff -u ../../codeigniter/framework/system/core/Loader.php application/tests/_ci_phpunit_test/replacing/core/Loader.php
+
 diff -u ../../codeigniter/framework/system/libraries/Upload.php application/tests/_ci_phpunit_test/replacing/libraries/Upload.php
