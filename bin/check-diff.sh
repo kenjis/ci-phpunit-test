e63b1f4c5379afc6858382ee825b6c344d7f9cf2
kenjis <kenji.uui@gmail.com>
2018-03-18 13:14:36 +0900

    chore: update check-diff.sh

--- bin/check-diff.sh
+++ bin/check-diff.sh
@@ -9,4 +9,7 @@ diff -u ../../codeigniter/framework/system/core/CodeIgniter.php application/test
 diff -u ../../codeigniter/framework/system/core/Input.php application/tests/_ci_phpunit_test/replacing/core/Input.php
 diff -u ../../codeigniter/framework/system/core/Loader.php application/tests/_ci_phpunit_test/replacing/core/Loader.php
 
+diff -u ../../codeigniter/framework/system/helpers/download_helper.php application/tests/_ci_phpunit_test/replacing/helpers/download_helper.php
+diff -u ../../codeigniter/framework/system/helpers/url_helper.php application/tests/_ci_phpunit_test/replacing/helpers/url_helper.php
+
 diff -u ../../codeigniter/framework/system/libraries/Upload.php application/tests/_ci_phpunit_test/replacing/libraries/Upload.php
