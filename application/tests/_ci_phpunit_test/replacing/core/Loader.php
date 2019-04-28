5ada9de0d54212fbdbdce6f0ea9ce3030b5635da
kenjis <kenji.uui@gmail.com>
2019-04-28 14:00:45 +0900

    docs: fix @copyright

--- application/tests/_ci_phpunit_test/replacing/core/Loader.php
+++ application/tests/_ci_phpunit_test/replacing/core/Loader.php
@@ -29,7 +29,7 @@
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (http://bcit.ca/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
  * @license	https://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
  * @since	Version 1.0.0
