ed92001dbd0669cd6b827c012c7668b4d4216a7a
Diego Ponce de León <dponce@devartis.com>
2019-10-18 17:55:29 -0300

    Allowed config files to be loaded from libraries in HMVC

---
+++ application/core/MY_Config.php
@@ -0,0 +1,6 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load the MX_Config class */
+require APPPATH."third_party/MX/Config.php";
+
+class MY_Config extends MX_Config {}
