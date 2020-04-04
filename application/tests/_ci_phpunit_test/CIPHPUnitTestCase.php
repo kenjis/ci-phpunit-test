e39b6e90f8027ab6f40377f9f250dfe4ea557e62
kenjis <kenji.uui@gmail.com>
2020-04-04 10:13:48 +0900

    fix: restore $strictRequestErrorCheck default value to true

--- application/tests/_ci_phpunit_test/CIPHPUnitTestCase.php
+++ application/tests/_ci_phpunit_test/CIPHPUnitTestCase.php
@@ -36,7 +36,7 @@ class CIPHPUnitTestCase extends PHPUnit_Framework_TestCase
 	 *
 	 * @var bool
 	 */
-	protected $strictRequestErrorCheck = false;
+	protected $strictRequestErrorCheck = true;
 
 	protected $restoreErrorHandler = false;
 
